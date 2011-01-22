13162ff80084383f433a1f4ddca5d0e441c59dbe
Fabian Neundorf <CommodoreFabianus@gmx.de>
2011-01-22 18:30:02 +0100

    *imported permissions system;

--- src/me/taylorkelly/mywarp/MyWarp.java
+++ src/me/taylorkelly/mywarp/MyWarp.java
@@ -1,16 +1,26 @@
 package me.taylorkelly.mywarp;
 
 import java.io.File;
+
 import java.util.logging.Logger;
 
 import org.bukkit.Server;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Priority;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.nijiko.Messaging;
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+
 public class MyWarp extends JavaPlugin{
+	
+	public static PermissionHandler permissions = new DefaultHandler();
+	
 	private WMPlayerListener playerListener;
 	public final String name = this.getDescription().getName();
 	public final String version = this.getDescription().getVersion();
@@ -28,6 +38,14 @@ public class MyWarp extends JavaPlugin{
 		if(new File("MyWarp").exists() && new File("MyWarp", "warps.db").exists()) {
 			updateFiles();
 		}
+		
+		Plugin test = this.getServer().getPluginManager().getPlugin("Permissions");
+
+		if(test != null) {
+			MyWarp.permissions = Permissions.Security;
+	    } else {
+	    	log.info(Messaging.bracketize(name) + " Permission system not found.");
+	    }		
 		
 		WarpList warpList = new WarpList(getServer());
 		playerListener = new WMPlayerListener(this, warpList);
