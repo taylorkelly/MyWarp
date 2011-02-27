2d5360a0f8efca3f1cd0fde17f5b6d65a4bd6f8b
Taylor Kelly <tkelly910@gmail.com>
2011-02-27 14:25:05 +0100

    fixed SignWarp problem

--- src/main/java/me/taylorkelly/mywarp/MWBlockListener.java
+++ src/main/java/me/taylorkelly/mywarp/MWBlockListener.java
@@ -9,17 +9,17 @@ import org.bukkit.event.block.BlockRightClickEvent;
 import org.bukkit.event.block.SignChangeEvent;
 
 public class MWBlockListener extends BlockListener {
+
     private WarpList list;
-    
-    
+
     public MWBlockListener(WarpList list) {
         this.list = list;
     }
-    
+
     @Override
     public void onBlockRightClick(BlockRightClickEvent event) {
         Block block = event.getBlock();
-        if(block.getState() instanceof Sign && SignWarp.isSignWarp((Sign) block.getState()) && WarpPermissions.signWarp(event.getPlayer())) {
+        if (block.getState() instanceof Sign && SignWarp.isSignWarp((Sign) block.getState()) && WarpPermissions.signWarp(event.getPlayer())) {
             SignWarp.warpSign((Sign) block.getState(), list, event.getPlayer());
         }
     }
@@ -27,12 +27,15 @@ public class MWBlockListener extends BlockListener {
     @Override
     public void onSignChange(SignChangeEvent event) {
         Player player = event.getPlayer();
-        if(SignWarp.isSignWarp(event) && WarpPermissions.createSignWarp(player)) {
-            player.sendMessage(ChatColor.AQUA + "Successfully created a sign warp");
 
-        } else {
-            player.sendMessage(ChatColor.RED + "You do not have permission to create a SignWarp");
-            event.setCancelled(true);
+        if (SignWarp.isSignWarp(event)) {
+            if (WarpPermissions.createSignWarp(player)) {
+                player.sendMessage(ChatColor.AQUA + "Successfully created a SignWarp");
+
+            } else {
+                player.sendMessage(ChatColor.RED + "You do not have permission to create a SignWarp");
+                event.setCancelled(true);
+            }
         }
     }
 }
