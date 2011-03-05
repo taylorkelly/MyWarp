f09e695c6e8fe9c5d9d1961e5a93be629ec6c4b8
Fabian Neundorf <CommodoreFabianus@gmx.de>
2011-03-05 15:40:54 +0100

    !remove error in info command, when the warp is invalid; !show permissions to sender; *better header of permission command;

--- src/de/xzise/xwarp/commands/PermissionsCommand.java
+++ src/de/xzise/xwarp/commands/PermissionsCommand.java
@@ -60,7 +60,11 @@ public class PermissionsCommand extends SubCommand {
 		}
 		
 		if (player != null)	{
-			sender.sendMessage("Your permissions:");
+			if (sender == player) {
+				sender.sendMessage("Your permissions:");
+			} else {
+				sender.sendMessage(player.getName() + " permissions:");
+			}
 			if (!MyWarp.permissions.useOfficial()) {
 				sender.sendMessage("(Use build in permissions!)");
 			}
@@ -68,7 +72,7 @@ public class PermissionsCommand extends SubCommand {
 				boolean hasPermission = MyWarp.permissions.permission(player, type);
 				if ((hasPermission && showGranted) || (!hasPermission && showDenied)) {
 					String message = (hasPermission ? ChatColor.GREEN : ChatColor.RED) + type.name + ": " + (hasPermission ? "Yes": "No");
-					player.sendMessage(message);
+					sender.sendMessage(message);
 				}
 			}
 			return true;
