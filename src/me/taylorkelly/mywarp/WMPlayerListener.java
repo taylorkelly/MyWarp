07c59172de123cf6202c102df5f9e10dbbc4499c
Fabian Neundorf <CommodoreFabianus@gmx.de>
2011-04-03 13:48:05 +0200

    *only rightclick now warps;

--- src/me/taylorkelly/mywarp/WMPlayerListener.java
+++ src/me/taylorkelly/mywarp/WMPlayerListener.java
@@ -3,6 +3,7 @@ package me.taylorkelly.mywarp;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.event.Event.Result;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerListener;
 
@@ -20,7 +21,7 @@ public class WMPlayerListener extends PlayerListener {
     @Override
     public void onPlayerInteract(PlayerInteractEvent event) {
         Block block = event.getClickedBlock();
-        if (block != null && event.getPlayer() != null && block.getState() instanceof Sign && MyWarp.permissions.permissionOr(event.getPlayer(), PermissionTypes.SIGN_WARP_GLOBAL, PermissionTypes.SIGN_WARP_INVITED, PermissionTypes.SIGN_WARP_OTHER, PermissionTypes.SIGN_WARP_OWN)) {
+        if (block != null && event.getAction() == Action.RIGHT_CLICK_BLOCK && block.getState() instanceof Sign && MyWarp.permissions.permissionOr(event.getPlayer(), PermissionTypes.SIGN_WARP_GLOBAL, PermissionTypes.SIGN_WARP_INVITED, PermissionTypes.SIGN_WARP_OTHER, PermissionTypes.SIGN_WARP_OWN)) {
             SignWarp signWarp = new SignWarp((Sign) block.getState());
             if (signWarp.warp(this.manager, event.getPlayer())) {
                 event.setUseInteractedBlock(Result.DENY);
