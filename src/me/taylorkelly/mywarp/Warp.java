3d81c09966dff75f8fcadfb3c60a3404ed1e0863
Fabian Neundorf <CommodoreFabianus@gmx.de>
2011-05-02 18:55:37 +0200

    +some more save blocks (e.g. water or flowers);

--- src/me/taylorkelly/mywarp/Warp.java
+++ src/me/taylorkelly/mywarp/Warp.java
@@ -9,6 +9,7 @@ import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import de.xzise.MinecraftUtil;
 import de.xzise.metainterfaces.LocationWrapper;
 import de.xzise.xwarp.EditorPermissions;
 import de.xzise.xwarp.Permissions;
@@ -130,12 +131,25 @@ public class Warp {
             Material lower = location.getBlock().getType();
             LocationWrapper.moveY(location, 1.0D);
             Material higher = location.getBlock().getType();
-            
-            //return (plugin.sm.throughBlocks.contains(mat1.getId())) && (plugin.sm.throughBlocks.contains(mat2.getId()));
-            return lower == Material.AIR && higher == Material.AIR;
+
+            return checkOpaqueMaterials(lower, higher);
         } else {
             return false;
         }
+    }
+    
+    private static boolean checkOpaqueMaterials(Material... materials) {
+        //TODO: Reorganize
+        return checkMaterials(materials, Material.AIR, Material.WATER, Material.STATIONARY_WATER, Material.SAPLING, Material.YELLOW_FLOWER, Material.RED_ROSE, Material.BROWN_MUSHROOM, Material.RED_MUSHROOM, Material.TORCH, Material.REDSTONE_TORCH_ON, Material.REDSTONE_TORCH_OFF, Material.REDSTONE_WIRE, Material.CROPS, Material.SIGN_POST, Material.LADDER, Material.RAILS, Material.WALL_SIGN, Material.LEVER, Material.STONE_PLATE, Material.WOOD_PLATE, Material.STONE_BUTTON, Material.SNOW, Material.WOODEN_DOOR, Material.PORTAL, Material.SUGAR_CANE_BLOCK, Material.IRON_DOOR, Material.POWERED_RAIL, Material.DETECTOR_RAIL, Material.CAKE_BLOCK);
+    }
+    
+    private static boolean checkMaterials(Material[] materials, Material... allowed) {
+        for (Material material : materials) {
+            if (!MinecraftUtil.contains(material, allowed)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public void setLocation(Positionable positionable) {
