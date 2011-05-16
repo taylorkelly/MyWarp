52eba881eb77a57572cfe471145b0ae9b851a002
Fabian Neundorf <CommodoreFabianus@gmx.de>
2011-05-16 11:54:45 +0200

    +missing PermissionDouble interface;

---
+++ src/de/xzise/xwarp/PermissionDouble.java
@@ -0,0 +1,11 @@
+package de.xzise.xwarp;
+
+import de.xzise.xwarp.wrappers.permission.PermissionTypes;
+
+public interface PermissionDouble {
+
+    public PermissionTypes getDefault();
+    
+    public PermissionTypes getAdmin();
+    
+}
